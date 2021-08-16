146593fdf3b1900c3b7076b845de4f7d55ea7a55
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-08-16 10:59:59 -0300

    Improve UI; Modify parents and images structure

--- gen.js
+++ gen.js
@@ -1,19 +1,43 @@
 import { MongoClient } from 'mongodb';
+import dayjs from 'dayjs-with-plugins';
+import 'dayjs/locale/pt-br.js';
+import * as cliProgress from 'cli-progress';
 import * as util from './utils.js';
 
-const uri = 'mongodb://user_app:pwd_app@localhost:27017/citizenDB';
-const client = new MongoClient(uri);
-
-const args = process.argv.slice(2);
-const iterations = args.length > 0 ? parseInt(args[0]) : 1;
-
 const run = async () => {
+  const uri = 'mongodb://user_app:pwd_app@localhost:27017/citizenDB';
+  const client = new MongoClient(uri);
+
+  const args = process.argv.slice(2);
+  const iterations = args.length > 0 ? parseInt(args[0]) : 1;
+
+  dayjs.locale('pt-br');
+  const start = new Date();
+
   try {
     console.log('Conectando...');
     await client.connect();
     await client.db('admin').command({ ping: 1 });
-    console.log('Conectado. Alocando recursos (pode demorar)...');
-    await cadastrar(client, iterations);
+    const citizens = client.db('citizenDB').collection('citizens');
+
+    console.log(`Gerando ${iterations} cidadãos...`);
+    const progressBar = new cliProgress.SingleBar(
+      {
+        hideCursor: true,
+        format:
+          '[{bar}] {percentage}% | {value}/{total} | {duration_formatted} (tempo restante estimado: {eta_formatted})',
+      },
+      cliProgress.Presets.rect
+    );
+    progressBar.start(iterations, 0);
+
+    await populateCitizensCollection(citizens, iterations, progressBar);
+
+    console.log(
+      `Tempo de execução: ${+new Date() - start} ms (${dayjs(start).toNow(
+        true
+      )})`
+    );
   } finally {
     await client.close();
   }
@@ -21,150 +45,30 @@ const run = async () => {
 
 run().catch(console.dir);
 
-const cadastrar = async () => {
+const populateCitizensCollection = async (
+  citizenCollection,
+  iterations,
+  progressBar
+) => {
+  const researchUnusedCpf = async (citizens) => {
+    const cpf = util.randomCPF();
+    return citizens
+      .findOne({ _id: cpf })
+      .then((found) => (found ? researchUnusedCpf(citizens) : cpf));
+  };
+
   const tasks = [];
   for (let i = 1; i <= iterations; i++) {
-    tasks.push(addNewCitizen(client.db('citizenDB')));
+    tasks.push(
+      researchUnusedCpf(citizenCollection).then((cpf) =>
+        citizenCollection
+          .insertOne(util.createCitizen(cpf))
+          .then(() => progressBar.increment())
+          .catch((err) => console.log(err))
+      )
+    );
   }
   return Promise.all(tasks).then(() => {
-    console.log('Finalizado.');
+    progressBar.stop();
   });
-};
-
-const addNewCitizen = async (citizenDB) => {
-  const citizens = citizenDB.collection('citizens');
-  const profilePhotos = citizenDB.collection('profilePhotos');
-  let cpf = '';
-
-  // Acha um CPF inexistente
-  while (true) {
-    cpf = util.randomCPF();
-    const citizenFound = await citizens.findOne({ cpf });
-    if (!citizenFound) break;
-  }
-
-  // Adiciona um número aleatório de fotos
-  const photos = [];
-  while (Math.random() > 0.3) {
-    photos.push(util.createPhotoOptional(cpf, photos.length.toString(16)));
-  }
-
-  // Cria um objeto de cidadão, sem foto
-  const gender = util.createGenderOptional() || undefined;
-  const name = util.createName(gender);
-  const socialName = util.createSocialNameOptional(gender) || undefined;
-
-  const foreigner = Math.random() > 0.95;
-  const nationalIdRG = !foreigner
-    ? util.createNationalIdRGOptional()
-    : undefined;
-  const nationalIdIssuer = util.createNationalIdIssuerOptional(nationalIdRG);
-  const nationalIdState = util.createNationalIdStateOptional(nationalIdIssuer);
-
-  let nationalId;
-  if (nationalIdRG || nationalIdIssuer || nationalIdState) {
-    nationalId = {};
-    nationalIdRG && (nationalId.rg = nationalIdRG);
-    nationalIdIssuer && (nationalId.issuer = nationalIdIssuer);
-    nationalIdState && (nationalId.state = nationalIdState);
-  }
-
-  const foreignerIdRNE = foreigner
-    ? util.createForeignerIdRNEOptional()
-    : undefined;
-
-  let foreignerId;
-  if (foreignerIdRNE) {
-    foreignerId = {};
-    foreignerIdRNE && (foreignerId.rne = foreignerIdRNE);
-  }
-
-  const voterIdTituloEleitor = !foreigner
-    ? util.createVoterIdTituloEleitorOptional()
-    : undefined;
-  const voterIdZone = util.createVoterIdZoneOptional(voterIdTituloEleitor);
-  const voterIdSection = util.createVoterIdSectionOptional(voterIdZone);
-
-  let voterId;
-  if (voterIdTituloEleitor || voterIdZone || voterIdSection) {
-    voterId = {};
-    voterIdTituloEleitor && (voterId.tituloEleitor = voterIdTituloEleitor);
-    voterIdZone && (voterId.zone = voterIdZone);
-    voterIdSection && (voterId.section = voterIdSection);
-  }
-
-  let documents;
-  if (nationalId || foreignerId || voterId) {
-    documents = {};
-    nationalId && (documents.nationalId = nationalId);
-    foreignerId && (documents.foreignerId = foreignerId);
-    voterId && (documents.voterId = voterId);
-  }
-
-  const mother = util.createMotherOptional() || undefined;
-  const father = util.createFatherOptional() || undefined;
-
-  let parents;
-  if (mother || father) {
-    parents = {};
-    mother && (parents.mother = mother);
-    father && (parents.father = father);
-  }
-
-  const birthDate = util.createBirthDateOptional() || undefined;
-  const birthPlace = util.createBirthPlaceOptional(foreigner) || undefined;
-
-  let birth;
-  if (birthDate || birthPlace || parents) {
-    birth = {};
-    birthDate && (birth.date = birthDate);
-    birthPlace && (birth.place = birthPlace);
-    parents && (birth.parents = parents);
-  }
-
-  const deathDate = util.createDeathDateOptional() || undefined;
-
-  let death;
-  if (deathDate) {
-    death = {};
-    death.date = deathDate;
-  }
-
-  const newCitizen = {};
-
-  newCitizen._id = cpf;
-  gender && (newCitizen.gender = gender);
-  newCitizen.name = name;
-  socialName && (newCitizen.socialName = socialName);
-
-  documents && (newCitizen.documents = documents);
-
-  birth && (newCitizen.birth = birth);
-  death && (newCitizen.death = death);
-
-  // Adiciona o cidadão
-  const result = await citizens.insertOne(newCitizen);
-  const citizenId = result.insertedId;
-  console.log(`Cidadão ${citizenId} adicionado.`);
-
-  // Adiciona as fotos no cadastro do cidadão
-  for await (let photo of photos) {
-    photo.owner = citizenId;
-    const result = await profilePhotos.insertOne(photo);
-    const photoId = result.insertedId;
-
-    // Vincula dados no cidadão
-    await citizens.updateOne({ _id: citizenId }, [
-      {
-        $set: {
-          images: {
-            profilePhotoUrl: photo.url,
-            profilePhoto: photoId,
-          },
-        },
-      },
-    ]);
-
-    console.log(`\tFoto ${photoId} adicionada em ${citizenId}.`);
-  }
 };
--- utils.js
+++ utils.js
@@ -171,10 +171,7 @@ const createPhotoOptional = (cpf, suffix) => {
   const newPhoto =
     getRandom(0, 4) === 0
       ? null
-      : {
-          format: 'jpeg',
-          url: `https://i.pravatar.cc/200?u=${cpf}.${suffix}`,
-        };
+      : `https://i.pravatar.cc/200?u=${cpf}.${suffix}`;
   return newPhoto;
 };
 
@@ -233,7 +230,8 @@ const createVoterIdSectionOptional = (zone) =>
 
 const createSocialNameOptional = (gender) => {
   const probability = Math.random();
-  if (gender?.includes('male')) return probability < 0.05 ? allNames.pickOne() : null;
+  if (gender?.includes('male'))
+    return probability < 0.05 ? allNames.pickOne() : null;
   else return probability < 0.8 ? allNames.pickOne() : null;
 };
 
@@ -293,17 +291,127 @@ const createBirthPlaceOptional = (foreigner) => {
 };
 
 const createMotherOptional = () =>
-  Math.random() < 0.95 ? createName('female') : null;
+  Math.random() < 0.95
+    ? {
+        name: createName('female'),
+      }
+    : null;
 
 const createFatherOptional = () =>
-  Math.random() < 0.8 ? createName('male') : null;
+  Math.random() < 0.8
+    ? {
+        name: createName('male'),
+      }
+    : null;
 
 const createDeathDateOptional = (birthDate) =>
   Math.random() > 0.95 ? createDate(birthDate) : null;
 
+const createCitizen = (cpf) => {
+  const gender = createGenderOptional() || undefined;
+  const name = createName(gender);
+  const socialName = createSocialNameOptional(gender) || undefined;
+
+  const foreigner = Math.random() > 0.95;
+  const nationalIdRG = !foreigner ? createNationalIdRGOptional() : undefined;
+  const nationalIdIssuer = createNationalIdIssuerOptional(nationalIdRG);
+  const nationalIdState = createNationalIdStateOptional(nationalIdIssuer);
+
+  let nationalId;
+  if (nationalIdRG || nationalIdIssuer || nationalIdState) {
+    nationalId = {};
+    nationalIdRG && (nationalId.rg = nationalIdRG);
+    nationalIdIssuer && (nationalId.issuer = nationalIdIssuer);
+    nationalIdState && (nationalId.state = nationalIdState);
+  }
+
+  const foreignerIdRNE = foreigner ? createForeignerIdRNEOptional() : undefined;
+
+  let foreignerId;
+  if (foreignerIdRNE) {
+    foreignerId = {};
+    foreignerIdRNE && (foreignerId.rne = foreignerIdRNE);
+  }
+
+  const voterIdTituloEleitor = !foreigner
+    ? createVoterIdTituloEleitorOptional()
+    : undefined;
+  const voterIdZone = createVoterIdZoneOptional(voterIdTituloEleitor);
+  const voterIdSection = createVoterIdSectionOptional(voterIdZone);
+
+  let voterId;
+  if (voterIdTituloEleitor || voterIdZone || voterIdSection) {
+    voterId = {};
+    voterIdTituloEleitor && (voterId.tituloEleitor = voterIdTituloEleitor);
+    voterIdZone && (voterId.zone = voterIdZone);
+    voterIdSection && (voterId.section = voterIdSection);
+  }
+
+  let documents;
+  if (nationalId || foreignerId || voterId) {
+    documents = {};
+    nationalId && (documents.nationalId = nationalId);
+    foreignerId && (documents.foreignerId = foreignerId);
+    voterId && (documents.voterId = voterId);
+  }
+
+  const mother = createMotherOptional() || undefined;
+  const father = createFatherOptional() || undefined;
+
+  let parents;
+  if (mother || father) {
+    parents = {};
+    mother && (parents.mother = mother);
+    father && (parents.father = father);
+  }
+
+  const birthDate = createBirthDateOptional() || undefined;
+  const birthPlace = createBirthPlaceOptional(foreigner) || undefined;
+
+  let birth;
+  if (birthDate || birthPlace || parents) {
+    birth = {};
+    birthDate && (birth.date = birthDate);
+    birthPlace && (birth.place = birthPlace);
+    parents && (birth.parents = parents);
+  }
+
+  const deathDate = createDeathDateOptional() || undefined;
+
+  let death;
+  if (deathDate) {
+    death = {};
+    death.date = deathDate;
+  }
+
+  // Cria um número aleatório de fotos
+  let photos = [];
+  while (Math.random() > 0.3) {
+    photos.push(createPhotoOptional(cpf, photos.length.toString(16)));
+  }
+  if (photos.length === 0) photos = undefined;
+
+  const newCitizen = {};
+
+  newCitizen._id = cpf;
+  gender && (newCitizen.gender = gender);
+  newCitizen.name = name;
+  socialName && (newCitizen.socialName = socialName);
+
+  documents && (newCitizen.documents = documents);
+
+  birth && (newCitizen.birth = birth);
+  death && (newCitizen.death = death);
+
+  photos && (newCitizen.images = { profilePhotos: photos });
+
+  return newCitizen;
+};
+
 export {
   randomCPF,
   getRandom,
+  createCitizen,
   createPhotoOptional,
   createNationalIdRGOptional,
   createNationalIdIssuerOptional,
